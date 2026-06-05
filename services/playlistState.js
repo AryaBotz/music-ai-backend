060ab5fa60e9ad7beb84d2687e0dc989780d16cb
AryaBotz <87101138+AryaBotz@users.noreply.github.com>
2026-06-05 22:45:29 +0700

    Update playlistState.js

--- services/playlistState.js
+++ services/playlistState.js
@@ -1,1 +1,41 @@
 
+let currentPlaylist = null;
+let currentIndex = 0;
+
+// SET playlist baru
+function setPlaylist(playlist) {
+  currentPlaylist = playlist;
+  currentIndex = 0;
+}
+
+// ambil track sekarang
+function getCurrentTrack() {
+  if (!currentPlaylist) return null;
+  return currentPlaylist.tracks[currentIndex];
+}
+
+// next track
+function nextTrack() {
+  if (!currentPlaylist) return null;
+
+  currentIndex++;
+
+  if (currentIndex >= currentPlaylist.tracks.length) {
+    currentIndex = 0;
+  }
+
+  return getCurrentTrack();
+}
+
+// optional: reset
+function reset() {
+  currentPlaylist = null;
+  currentIndex = 0;
+}
+
+module.exports = {
+  setPlaylist,
+  getCurrentTrack,
+  nextTrack,
+  reset
+};
