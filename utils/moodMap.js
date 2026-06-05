d8f20579b189691c27b36f7a456fc5fcbe0a0fe8
AryaBotz <87101138+AryaBotz@users.noreply.github.com>
2026-06-05 22:24:13 +0700

    Add moodMap.js for music genre categorization

---
+++ utils/moodMap.js
@@ -0,0 +1,9 @@
+const moodMap = {
+  calm: ["lofi", "ambient"],
+  happy: ["pop", "dance"],
+  sad: ["acoustic", "piano"],
+  focus: ["instrumental", "lofi"],
+  party: ["edm", "electronic"]
+};
+
+module.exports = moodMap;
