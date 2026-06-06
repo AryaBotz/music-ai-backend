19b8d3c189d71a3aad2701fdbd69208fa1a8bf44
AryaBotz <87101138+AryaBotz@users.noreply.github.com>
2026-06-06 14:56:37 +0700

    Update playlist.js

--- services/playlist.js
+++ services/playlist.js
@@ -1,70 +1,20 @@
-let state = {
-  playlist: [],
-  index: 0
-};
+const moodMap = require("../utils/moodMap");
+const { searchTracks } = require("./jamendo");
 
-function setPlaylist(tracks = []) {
-  state.playlist = Array.isArray(tracks)
-    ? tracks
-    : [];
+async function buildPlaylist(mood) {
+  const genres = moodMap[mood] || ["pop"];
 
-  state.index = 0;
+  const tag = genres[0];
 
-  console.log(
-    "Playlist loaded:",
-    state.playlist.length
-  );
-}
+  const tracks = await searchTracks(tag);
 
-function getCurrent() {
-  if (state.playlist.length === 0) {
-    return null;
-  }
-
-  return state.playlist[state.index];
-}
-
-function next() {
-  if (state.playlist.length === 0) {
-    return null;
-  }
-
-  state.index++;
-
-  if (state.index >= state.playlist.length) {
-    state.index = 0;
-  }
-
-  return state.playlist[state.index];
-}
-
-function previous() {
-  if (state.playlist.length === 0) {
-    return null;
-  }
-
-  state.index--;
-
-  if (state.index < 0) {
-    state.index = state.playlist.length - 1;
-  }
-
-  return state.playlist[state.index];
-}
-
-function getPlaylist() {
-  return state.playlist;
-}
-
-function getIndex() {
-  return state.index;
+  return {
+    mood,
+    tag,
+    tracks
+  };
 }
 
 module.exports = {
-  setPlaylist,
-  getCurrent,
-  next,
-  previous,
-  getPlaylist,
-  getIndex
+  buildPlaylist
 };
